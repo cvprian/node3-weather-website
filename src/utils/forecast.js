074e8aa2673be860eeb81ccd69ae1432eedd1a5e
Cyprian Panicz <panicz.cyprian@gmail.com>
2020-05-13 12:43:22 +0200

    Add humidity information

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -14,7 +14,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined);
         } else {
-            callback(undefined, `${body.current.weather_descriptions[0]}. It is ${body.current.temperature} degrees out. It feels like it is ${body.current.feelslike} degrees. Pressure is ${body.current.pressure}`);
+            callback(undefined, `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out. Pressure is ${body.current.pressure} and the humidity is ${body.current.humidity}%`);
         }
     })
 }
