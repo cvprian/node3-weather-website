d9b9a5fc5fb1f67322f179ea105affdf53d60340
Cyprian Panicz <panicz.cyprian@gmail.com>
2020-05-13 12:35:20 +0200

    Add pressure information to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -14,7 +14,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined);
         } else {
-            callback(undefined, `${body.current.weather_descriptions[0]}. It is ${body.current.temperature} degrees out. It feels like it is ${body.current.feelslike} degrees`);
+            callback(undefined, `${body.current.weather_descriptions[0]}. It is ${body.current.temperature} degrees out. It feels like it is ${body.current.feelslike} degrees. Pressure is ${body.current.pressure}`);
         }
     })
 }
